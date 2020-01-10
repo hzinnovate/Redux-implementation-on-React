7f6b09ee108413a1b0dadff81c91f3f53d1e1701
syedzaidi95 <hasnainzaidi95@gmail.com>
2020-01-11 01:52:18 +0500

    redux

--- src/App.js
+++ src/App.js
@@ -2,25 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {store, persistor} from './store';
+import { Provider } from 'react-redux';
+import Login from './screens/Login';
+import Profile from './screens/Profile';
+import persistGate from 
+
+class App extends React.Component {
+  state = {
+    showProfile: false
+  }
+
+  afterLogin() {
+    this.setState({showProfile: true});
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+
+            {!this.state.showProfile ? <Login afterLogin={this.afterLogin.bind(this)}/> : <Profile />}
+
+          </header>
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
